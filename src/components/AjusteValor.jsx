abea402b1a920fbc51972517e28a87bcc1b9ce65
Harry Hernandez Arias <132928917+HarryHndz@users.noreply.github.com>
2023-11-21 08:12:35 -0600

    ya ahora si

--- src/components/AjusteValor.jsx
+++ src/components/AjusteValor.jsx
@@ -36,7 +36,7 @@ export const Formulario = (Cuadro) => {
             from-blue-900 to-violet-500 text-center">Factor de ajuste de valor</h1>
                 </div>
                 <div className="flex justify-center items-center my-5">
-                    <caption class="caption-bottom">*Responda las 14 preguntas indicadas y establezca un valor a cada una desde 1 (no importante) a 6 (absolutamente esencial)</caption>
+                    <caption class="caption-bottom">*Responda las 14 preguntas indicadas y establezca un valor a cada una desde 0 (no importante) a 5 (absolutamente esencial)</caption>
                 </div>
                 <div className="flex justify-center items-center my-5">
                 <form onSubmit={ev => {
@@ -44,13 +44,16 @@ export const Formulario = (Cuadro) => {
                     const preguntasVacias = [];
                     let total = 0
                     for (let i = 1; i <= 14; i++) {
-                        const value = Number(ev.target[`p${i}`].value)
-                        if (!value) {
-                            preguntasVacias.push(`Pregunta ${i}`);
-                            
+                        let longi = ev.target[`p${i}`].value
+                        if(longi.length === 1){
+                            const value = Number(longi)
+                            total += value
+                        }else{
+                            preguntasVacias.push(`Pregunta ${i}`)
                         }
+
                         
-                        total += value
+                        
                     }
                     if (preguntasVacias.length > 0) {
                         setAlertMessage(`Los siguientes campos están vacíos: ${preguntasVacias.join(', ')}`)
@@ -62,38 +65,38 @@ export const Formulario = (Cuadro) => {
                 }}>
                     <label>1. ¿El sistema requiere respaldo y recuperación confiables?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p1" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 
+                    <input type="number" min='0' max='5'  name="p1" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 
                     focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>2. ¿Se requieren comunicaciones de datos especializadas para 
                     transferir información hacia o desde la aplicación? </label>
                     <br />
-                    <input type="number" min="1" max="6" name="p2" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p2" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>3. ¿Existen funciones de procesamiento distribuidas?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p3" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5'  name="p3" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>4. ¿El desempeño es crucial?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p4" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p4" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>5. ¿El sistema correrá en un entorno operativo existente enormemente utilizado?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p5" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p5" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>6. ¿El sistema requiere entrada de datos en línea?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p6" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p6" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
@@ -101,52 +104,52 @@ export const Formulario = (Cuadro) => {
                     transacción de entrada se construya sobre múltiples 
                     pantallas u operaciones?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p7" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p7" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>8. ¿Los ALI se actualizan en línea?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p8" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number"min='0' max='5' name="p8" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>9. ¿Las entradas, salidas, archivos o consultas son 
                     complejos?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p9" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p9" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>10. ¿El procesamiento interno es complejo?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p10" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p10" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>11. ¿El código se diseña para ser reutilizable?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p11" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p11" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"/>
                     <br />
 
                     <label>12. ¿La conversión y la instalación se incluyen en el 
                     diseñ?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p12" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p12" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>13. ¿El sistema se diseña para instalaciones múltiples en 
                     diferentes organizaciones?</label>
                     <br />
-                    <input type="number" min="1" max="6" name="p13" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p13" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
 
                     <label>14. ¿La aplicación se diseña para facilitar el cambio y su 
                     uso por parte del usuario?</label>
-                    <input type="number" min="1" max="6" name="p14" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
+                    <input type="number" min='0' max='5' name="p14" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                     placeholder-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                     <br />
                     {alertMessage && (
